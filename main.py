bf67ae45410527d632c8b59821cb63c9a2c79a93
Michael Byers <mikebyers24@gmail.com>
2024-08-05 10:08:46 -0400

    added website, email, and password to external .txt file on clicking add button.

--- main.py
+++ main.py
@@ -9,35 +9,45 @@ window = Tk()
 window.title('Password Manager')
 window.config(padx=50, pady=50, bg='white')
 
+
+def save_data():
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+    with open('data.txt', 'w') as file:
+        file.write(f'{website} | {email} | {password}')
+
+
 canvas = Canvas(width=200, height=200, bg='white', highlightthickness=0)
 bg_img = PhotoImage(file='logo.png')
 canvas.create_image(100, 100, image=bg_img)
-canvas.grid(column=1, row=0)
+canvas.grid(column=1, row=0, pady=(0, 20))
 
 website_label = Label(text='Website:', bg='white', fg='black')
-website_label.grid(column=0, row=1)
+website_label.grid(column=0, row=1, pady=5)
 
 website_entry = Entry(width=35, bg='white', fg='black', highlightthickness=1)
-website_entry.grid(column=1, row=1, columnspan=2)
+website_entry.grid(column=1, row=1, columnspan=2, pady=5)
+website_entry.focus()
 
 email_label = Label(text='Email/Username:', bg='white', fg='black')
-email_label.grid(column=0, row=2)
+email_label.grid(column=0, row=2, pady=5)
 
 email_entry = Entry(width=35, bg='white', fg='black', highlightthickness=1)
-email_entry.grid(column=1, row=2, columnspan=2)
+email_entry.insert(0, 'mikebyers24@gmail.com')
+email_entry.grid(column=1, row=2, columnspan=2, pady=5)
 
 password_label = Label(text="Password:", bg='white', fg='black')
-password_label.grid(column=0, row=3)
+password_label.grid(column=0, row=3, pady=5)
 
 password_entry = Entry(width=23, bg='white', fg='black', highlightthickness=1)
-password_entry.grid(column=1, row=3)
+password_entry.grid(column=1, row=3, pady=5)
 
 generate_button = Button(text='Generate', highlightbackground='white', highlightthickness=1)
-generate_button.grid(column=2, row=3)
+generate_button.grid(column=2, row=3, pady=4)
 
-add_button = Button(text='Add', width=34, highlightbackground='white', highlightthickness=1)
+add_button = Button(text='Add', width=34, highlightbackground='white', highlightthickness=1, command=save_data)
 add_button.grid(column=1, row=4, columnspan=2)
-
 
 
 window.mainloop()
