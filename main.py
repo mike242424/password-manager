2b02a9e903a8b10e0927f3c0f4f9d77377beb6bb
Michael Byers <mikebyers24@gmail.com>
2024-08-06 10:14:44 -0400

    comments.

--- main.py
+++ main.py
@@ -4,6 +4,27 @@ from password_generator import generate_password
 import pyperclip
 import json
 
+# ---------------------------- SEARCH WEBSITE ------------------------------- #
+
+
+def search_website():
+    try:
+        with open('data.json', 'r') as file:
+            data = json.load(file)
+    except FileNotFoundError:
+        messagebox.showerror("Error", "Data File Not Found")
+    else:
+        website = website_entry.get()
+        if website in data:
+            messagebox.showinfo(website, f'Website: {website}\nEmail: '
+                                         f'{data[website]['email']}\nPassword: {data[website]['password']}')
+            password_entry.delete(0, END)
+            password_entry.insert(0, data[website]['password'])
+
+        else:
+            messagebox.showerror("Error", "Website Not Found")
+
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 
@@ -57,13 +78,16 @@ bg_img = PhotoImage(file='logo.png')
 canvas.create_image(100, 100, image=bg_img)
 canvas.grid(column=1, row=0, pady=(0, 20))
 
+# Website
 website_label = Label(text='Website:', bg='white', fg='black')
 website_label.grid(column=0, row=1, pady=5)
 
-website_entry = Entry(width=35, bg='white', fg='black', highlightthickness=1)
-website_entry.grid(column=1, row=1, columnspan=2, pady=5)
+website_entry = Entry(width=23, bg='white', fg='black', highlightthickness=1)
+website_entry.grid(column=1, row=1, pady=5)
 website_entry.focus()
 
+
+# Email
 email_label = Label(text='Email/Username:', bg='white', fg='black')
 email_label.grid(column=0, row=2, pady=5)
 
@@ -71,13 +95,19 @@ email_entry = Entry(width=35, bg='white', fg='black', highlightthickness=1)
 email_entry.insert(0, 'mikebyers24@gmail.com')
 email_entry.grid(column=1, row=2, columnspan=2, pady=5)
 
+# Password
 password_label = Label(text="Password:", bg='white', fg='black')
 password_label.grid(column=0, row=3, pady=5)
 
 password_entry = Entry(width=23, bg='white', fg='black', highlightthickness=1)
 password_entry.grid(column=1, row=3, pady=5)
 
-generate_button = Button(text='Generate', highlightbackground='white', highlightthickness=1, command=generate_my_password)
+# Buttons
+search_button = Button(text='Search', highlightbackground='white', highlightthickness=1, command=search_website)
+search_button.grid(column=2, row=1, pady=4)
+
+generate_button = Button(text='Generate', highlightbackground='white',
+                         highlightthickness=1, command=generate_my_password)
 generate_button.grid(column=2, row=3, pady=4)
 
 add_button = Button(text='Add', width=34, highlightbackground='white', highlightthickness=1, command=save_data)
